b90a24bba849adfc6797002292dec081728e50ee
yangjiaqi <jiaqi.yang@veriti@xyz>
2025-08-28 14:57:35 +0800

    add node log and reformat conde style

--- api/core/workflow/nodes/llm/node.py
+++ api/core/workflow/nodes/llm/node.py
@@ -251,8 +251,8 @@ class LLMNode(Node[LLMNodeData]):
             )
 
             # 输出日志：调用模型前的参数以及 模型返回的内容
-            logger.info(f"调用模型前的参数: {prompt_messages}")
-            logger.info(f"调用模型后的参数: {generator}")
+            logger.info("调用模型前的参数: %s", prompt_messages)
+            logger.info("调用模型后的参数: %s", generator)
 
 
 
@@ -306,7 +306,7 @@ class LLMNode(Node[LLMNodeData]):
                 "usage": jsonable_encoder(usage),
                 "finish_reason": finish_reason,
             }
-            logger.info(f"模型返回的内容: {outputs}")
+            logger.info("模型返回的内容: %s", outputs)
             if structured_output:
                 outputs["structured_output"] = structured_output.structured_output
             if self._file_outputs:
